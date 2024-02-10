40e1c6783bf4503121c0d1c1ed3473571383488b
Kaustubh Dixit <92023578+kaustubhdidit@users.noreply.github.com>
2024-02-10 21:28:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    // origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
